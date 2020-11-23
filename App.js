cdb306b6d63c0d3b16b86f88b9c23515050ae5c0
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-23 22:50:40 +0900

    Weather API

--- App.js
+++ App.js
@@ -2,15 +2,33 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "4f8365e56c2450b03c739d24a020ccab";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       //에러확인법
-      //throw Error();
+      // throw Error();
       await Location.requestPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {
+        coords: { longitude, latitude },
+      } = await Location.getCurrentPositionAsync();
+      //send to api and get weather
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you", "so sad");
     }
@@ -20,6 +38,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
