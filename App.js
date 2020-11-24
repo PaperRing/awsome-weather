0870ed0682f63b3ed087d22af92a8e0273010ef7
junghee <junghee@jungheeui-MacBookPro.local>
2020-11-24 22:20:51 +0900

    Getting the Temperature and Condtion Names

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
+import Weather from "./Weather";
 import * as Location from "expo-location";
 import axios from "axios";
 
@@ -10,27 +11,31 @@ export default class extends React.Component {
   state = {
     isLoading: true,
   };
-
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    console.log(data);
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
   };
 
   getLocation = async () => {
     try {
-      //에러확인법
-      // throw Error();
       await Location.requestPermissionsAsync();
       const {
-        coords: { longitude, latitude },
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      //send to api and get weather
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error) {
-      Alert.alert("Can't find you", "so sad");
+      Alert.alert("Can't find you.", "So sad");
     }
   };
 
@@ -38,7 +43,11 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
